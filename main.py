026a09fbdcc5f4f6a96ca39590f17bfc835969ad
Geoff Riley <geoffr@adaso.com>
2026-01-03 16:29:36 +0000

    a little housework: refactored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
-
 from pathlib import Path
+from stats import get_num_words
 
 BOOK_ROOT = Path("books")
 
@@ -9,13 +9,10 @@ def get_book_text(filepath: Path) -> str:
         file_contents = f.read()
     return file_contents
 
-def count_words(text: str) -> int:
-    return len(list(text.split()))
-
 def main():
     book = "frankenstein.txt"
     text = get_book_text(BOOK_ROOT / book)
-    num_words = count_words(text)
+    num_words = get_num_words(text)
     print(f"Found {num_words} total words")
 
 main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python3
+
+def get_num_words(text: str) -> int:
+    return len(list(text.split()))
