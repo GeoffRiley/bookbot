65f1366145dafa13c156e7fae78066afb8e2c432
Geoff Riley <geoffr@adaso.com>
2026-01-03 17:27:54 +0000

    stats report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 from pathlib import Path
-from stats import get_num_words
+from stats import get_num_words, char_frequency, sort_dictionary
 
 BOOK_ROOT = Path("books")
 
@@ -9,11 +9,34 @@ def get_book_text(filepath: Path) -> str:
         file_contents = f.read()
     return file_contents
 
-def main():
-    book = "frankenstein.txt"
+def print_report(book):
     text = get_book_text(BOOK_ROOT / book)
     num_words = get_num_words(text)
+    freqs = char_frequency(text)
+    char_count = sort_dictionary(freqs)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {BOOK_ROOT / book}")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for r in char_count:
+        ch = r["char"]
+        fr = r["num"]
+        if ch.isalpha():
+            print(f"{ch}: {fr}")
+    print("============= END ===============")
+
+
+def main():
+    book = "frankenstein.txt"
+    # text = get_book_text(BOOK_ROOT / book)
+    # num_words = get_num_words(text)
+    # print(f"Found {num_words} total words")
+    # freqs = char_frequency(text)
+    # print(freqs)
+    # tmp = sort_dictionary(freqs)
+    # print(tmp)
+    print_report(book)
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,4 +1,15 @@
 #! /usr/bin/env python3
+from collections import Counter
 
 def get_num_words(text: str) -> int:
     return len(list(text.split()))
+
+def char_frequency(text: str) -> dict[str, int]:
+    counts = Counter(text.lower())
+    return dict(counts)
+
+def sort_dictionary(letter_list: dict[str, int])->list[dict[str,str|int]]:
+    new_dict = [{"char": ch, "num": n} for ch, n in letter_list.items()]
+    sorted_list = sorted(new_dict, reverse=True, key=lambda x: x["num"])
+    return sorted_list
+
